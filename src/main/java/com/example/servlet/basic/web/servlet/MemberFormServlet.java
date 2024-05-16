2e1fa008f30b253daa42889d3488a17f828f1587
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-05-16 21:24:55 +0900

    servlet

--- src/main/java/com/example/servlet/basic/web/servlet/MemberFormServlet.java
+++ src/main/java/com/example/servlet/basic/web/servlet/MemberFormServlet.java
@@ -1,13 +1,13 @@
 package com.example.servlet.basic.web.servlet;
 
 import com.example.servlet.basic.repository.MemberRepository;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "memberformServlet", urlPatterns = "/servlet/members/new-form")
 public class MemberFormServlet extends HttpServlet {
@@ -17,23 +17,9 @@ public class MemberFormServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.setCharacterEncoding("utf-8");
 
-        PrintWriter w = response.getWriter();
-
-        w.write("<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <title>Title</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
-                "    username: <input type=\"text\" name=\"username\" />\n" +
-                "    age:      <input type=\"text\" name=\"age\" />\n" +
-                " <button type=\"submit\">전송</button>\n" + "</form>\n" +
-                "</body>\n" +
-                "</html>\n");
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request,response); // 다른 서블릿이나 JSP로 이동할 수 있는 기능, 서버 내부에서 다시 호출이 발생
     }
 }
